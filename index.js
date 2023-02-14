b19bd3f81b111441c6a03a117ccddf6d18dd402f
Geraldo Reis <reis.geraldomateus@gmail.com>
2023-02-14 19:31:21 -0300

    log all request

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT;
+
+app.use(express.json())
+
+app.all('*', ({ headers, body, query, path, method, cookies, protocol, hostname }, response) => {
+  const responseJson = {
+    timestamp: new Date(),
+    headers,
+    body,
+    cookies,
+    query,
+    method,
+    path,
+    hostname,
+    protocol,
+  }
+
+  console.log(JSON.stringify(responseJson));
+
+  response.json(responseJson);
+})
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`)
+})
